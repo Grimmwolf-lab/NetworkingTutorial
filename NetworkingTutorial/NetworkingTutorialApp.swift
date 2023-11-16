452382326c6fbbb35755509419883ffee2255fe9
Kailash Bora <kailash.bora@synchronoss.com>
2023-11-16 09:29:40 +0530

    Initial Commit

---
+++ NetworkingTutorial/NetworkingTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingTutorialApp.swift
+//  NetworkingTutorial
+//
+//  Created by Kailash Bora on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
